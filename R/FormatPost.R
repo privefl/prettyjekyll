81049c1ac513f6abbdfede138f0f97bb32d39633
Florian <florian.prive.21@gmail.com>
2017-05-13 16:00:36 +0200

    simplify code

--- R/FormatPost.R
+++ R/FormatPost.R
@@ -1,24 +1,24 @@
-#' @title Format an R Markdown post for a Jekyll blog
-#' @description Format an R Markdown Pretty Document (package
-#' prettydoc) post for a Jekyll blog
-#' and place the resulting Markdown post in directory '_posts'.
-#' @param rmd The name (path) to specific Rmd file to convert.
-#' @param knitr.files.dir The permanent directory where
-#' the files like plots are placed.
-#' @param tmp.dir The temporary directory for the temporary html file.
-#' @return The name (path) of the new Markdown post.
-#' @name FormatPost
-#' @export
-FormatPost <- function(rmd,
-                       knitr.files.dir = "knitr_files",
-                       tmp.dir = "_built") {
-  # rmd: name/path to specific Rmd file to convert
-  # knitr.files.dir: The permanent directory where the files like plots
-  #                  are put
-  # tmp.dir: temporary directory for the temporary html file
+################################################################################
 
-  # verif
-  if (tools::file_ext(rmd) != "Rmd") stop("Input shoud be an R Markdown file")
+new_yaml_header <- function(yaml) {
+
+  if (is.null(yaml$title)) stop("Your document should have an YAML title.")
+  if (is.null(yaml$date)) stop("Your document should have an YAML date.")
+
+  glue::glue(
+    "---",
+    "title: {yaml$title}",
+    `if`(is.null(yaml$author), "", "author: {yaml$author}"),
+    "date: {yaml$date}",
+    "layout: post",
+    "---",
+    .sep = "\n"
+  )
+}
+
+################################################################################
+
+format_html <- function(rmd, knitr.files.dir, tmp.dir) {
 
   # knit
   html <- rmarkdown::render(rmd,
@@ -28,67 +28,83 @@ FormatPost <- function(rmd,
                             clean = FALSE,
                             encoding = "UTF-8")
 
-  # Read rmd
-  lines.rmd <- readLines(rmd, encoding = "UTF-8")
-  lines.html <- readLines(html, encoding = "UTF-8")
+  # Read html
+  lines.html <- readLines(html)
 
-  # get main section
+  # get main section and scripts
+  scripts <- lines.html[grep("<script src=", lines.html, fixed = TRUE)]
   section.begin <- grep("<section class=\"main-content\">",
                         lines.html, fixed = TRUE)
   section.ends <- grep("</section>", lines.html, fixed = TRUE)
   section.end <- min(section.ends[section.ends > section.begin])
-  scripts <- grep("<script src=", lines.html, fixed = TRUE)
-  lines.html <- lines.html[c(scripts, section.begin:section.end)]
+  lines.html <- lines.html[section.begin:section.end]
 
-  # get yaml header (delimited by the first '---')
-  header.rmd <- grep("---", x = lines.rmd, fixed = TRUE)[2]
-  if (is.na(header.rmd)) stop("Your document should have an YAML header")
+  # Change path of figures and images
+  patterns <- c(
+    sub("\\.Rmd$", "_files", basename(rmd)),
+    getwd(),
+    gsub("\\", "\\\\", normalizePath(getwd()), fixed = TRUE)
+  )
+  replacements <- c(
+    file.path("{{ site.url }}{{ site.baseurl }}", knitr.files.dir, patterns[1]),
+    "{{ site.url }}{{ site.baseurl }}",
+    "{{ site.url }}{{ site.baseurl }}"
+  )
+  for (i in seq_along(patterns)) {
+    lines.html <- gsubfn::gsubfn(patterns[i], replacements[i], lines.html)
+  }
+  # And move figure objects
+  if (!dir.exists(knitr.files.dir)) dir.create(knitr.files.dir)
+  file.copy(from = file.path(tmp.dir, patterns[1]), to = knitr.files.dir,
+            overwrite = TRUE, recursive = TRUE)
 
-  # get 'title', 'date' and 'layout' headers
-  title <- grep("title:", lines.rmd[1:header.rmd], fixed = TRUE)
-  if (length(title) == 0) stop("Your document should have an YAML title")
-  date <- grep("date:", lines.rmd[1:header.rmd], fixed = TRUE)
-  if (length(date) == 0) stop("Your document should have an YAML date")
-  author <- grep("author:", lines.rmd[1:header.rmd], fixed = TRUE)
-  new.header <- paste("---", lines.rmd[title], lines.rmd[author], lines.rmd[date],
-                      "layout: post", "---", "", sep = "\n")
+  c(scripts, "", lines.html)
+}
+
+################################################################################
+
+#' Format an R Markdown post for a Jekyll blog
+#'
+#' Format an R Markdown Pretty Document (package prettydoc) post for a Jekyll
+#' blog and place the resulting Markdown post in directory '_posts'.
+#'
+#' @param rmd The name (path) to specific Rmd file to convert.
+#' @param knitr.files.dir The permanent directory where
+#' the files like plots are placed.
+#' @param tmp.dir The temporary directory for the temporary html file.
+#' @param date.format Format of the date used in the YAML header.
+#' Default corresponds for example to "August 15, 2016".
+#' @param date.locale A locale object, defining the defaults of a country.
+#'
+#' @return The name (path) of the new Markdown post.
+#'
+#' @seealso readr::parse_date
+#'
+#' @export
+FormatPost <- function(rmd,
+                       knitr.files.dir = "knitr_files",
+                       tmp.dir = "_built",
+                       date.format = "%B %d, %Y",
+                       date.locale = readr::locale(tz = "US/Central")) {
+
+  # verif
+  if (tools::file_ext(rmd) != "Rmd") stop("Input shoud be an R Markdown file")
+
+  # get YAML header
+  yaml <- rmarkdown::yaml_front_matter(rmd)
+  yaml.new <- new_yaml_header(yaml)
+
+  # get html lines
+  lines.html <- format_html(rmd, knitr.files.dir, tmp.dir)
 
   # get the right name output format
-  line.title <- sub("title:", "", lines.rmd[title], fixed = TRUE)
-  line.title <- gsub('\"', "", line.title, fixed = TRUE)
-  suffix <- gsub("[ ]{1,}", "-", line.title)
-  md.path <- file.path("_posts", paste0(Sys.Date(), suffix, ".md"))
-
-  # Change path to figures
-  rmd.base <- sub("\\.Rmd$", "", basename(rmd))
-  pattern <-  paste0(rmd.base, "_files")
-  tmp <- gsubfn::strapply(lines.html, pattern)
-  ind <- which(!sapply(tmp, is.null))
-  if (length(ind > 0)) {
-    for (i in ind) {
-      lines.html[i] <-
-        gsubfn::gsubfn(pattern, file.path("{{ site.url }}{{ site.baseurl }}", knitr.files.dir,
-                                          tmp[[i]][1]), lines.html[i])
-    }
-    dir.source <- file.path(tmp.dir, pattern)
-    if (!dir.exists(knitr.files.dir)) dir.create(knitr.files.dir)
-    file.copy(from = dir.source, to = knitr.files.dir,
-              overwrite = TRUE, recursive = TRUE)
-  }
-
-  # Change path to images
-  pattern <- getwd()
-  tmp <- gsubfn::strapply(lines.html, pattern)
-  ind <- which(!sapply(tmp, is.null))
-  for (i in ind) {
-    lines.html[i] <-
-      gsubfn::gsubfn(pattern, "{{ site.url }}{{ site.baseurl }}", lines.html[i])
-  }
-
+  md.path <- file.path("_posts", sprintf(
+    "%s-%s.md",
+    readr::parse_date(yaml$date, format = date.format, locale = date.locale),
+    gsub("[ ]{1,}", "-", yaml$title)))
   # create file with new lines
   if (!dir.exists("_posts")) dir.create("_posts")
-  writeLines(c(new.header, "", "", lines.html),
-             md.path, useBytes = TRUE)
+  writeLines(c(yaml.new, "", lines.html), md.path, useBytes = FALSE)
 
   # remove temporary directory and clean build files
   unlink(tmp.dir, recursive = TRUE)
@@ -98,3 +114,4 @@ FormatPost <- function(rmd,
   return(md.path)
 }
 
+################################################################################
